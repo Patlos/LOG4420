6bd232c32c8752f1776f8075fa0c15dfbac82da5
jjliang1005 <jia-jing.liang@insa-lyon.fr>
2013-11-15 00:07:40 -0500

    simple applicative form created

--- Handler/InitJeu.hs
+++ Handler/InitJeu.hs
@@ -3,34 +3,43 @@ module Handler.InitJeu where
 import 		Import
 
 data Person = Person
-	{
-		habilete	:: Integer,
-		endurance	:: Integer,
-		nbOr		:: Integer,
-		disciplines	:: [Text],
-		objets		:: [Text]
+	{	weaponMastery	:: Maybe Bool
+	,	animalControl	:: Maybe Bool
+	,	curing			:: Maybe Bool
+	,	invisibility	:: Maybe Bool
+	,	huntMastery		:: Maybe Bool
+	,	pathsmanship	:: Maybe Bool
+	,	psiSurge		:: Maybe Bool
+	,	psiScreen		:: Maybe Bool
+	,	nexus			:: Maybe Bool
+	,	divination		:: Maybe Bool
 	}deriving Show
 	
---personForm :: Html -> MForm Handler (FormResult Person, Widget)
-	              -- <a href=@{PageR}/#{pageCourante} class="button">Retourner à la page : #{pageCourante}
-	              -- <a href=@{InitJeuR} class="button">Recommencer la partie!
-	              -- partieCourante <- lookupSession "partieCourante"
-	   			-- pageCourante <- lookupSession "pageCourante"
+personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm = renderDivs $ Person
+    <$> aopt checkBoxField "La science des armes" Nothing
+	<*> aopt checkBoxField "Contrôle animal" Nothing
+	<*> aopt checkBoxField "Science médical" Nothing
+	<*> aopt checkBoxField "Invisibilité" Nothing
+	<*> aopt checkBoxField "Art de la chasse" Nothing
+	<*> aopt checkBoxField "Exploration" Nothing
+	<*> aopt checkBoxField "Foudroiement psychique" Nothing
+	<*> aopt checkBoxField "Écran psychique" Nothing
+	<*> aopt checkBoxField "Nexus" Nothing
+	<*> aopt checkBoxField "Intuition" Nothing
 
 getInitJeuR :: Handler Html
-getInitJeuR = defaultLayout  $ do
-			setTitle "Castle Death"
-			addScriptRemote "//code.jquery.com/jquery-1.10.1.js"
-			toWidget [julius|
-          		$("#jeu").addClass("current");
-        	|]
-			$(widgetFile "navigation")
-			$(widgetFile "person_creation")
-			$(widgetFile "footer")
-			$(widgetFile "main")
+getInitJeuR = do
+(widget, enctype) <- generateFormPost personForm
+defaultLayout $ do
+setTitle "Castle Death"
+addScriptRemote "//code.jquery.com/jquery-1.10.1.js"
+toWidget [julius|
+	$("#jeu").addClass("current");
+|]
+$(widgetFile "navigation")
+$(widgetFile "personCreation")
+$(widgetFile "footer")
+$(widgetFile "main")
 
 
-			
-
-postInitJeuR :: Handler Html
-postInitJeuR = error "Not yet implemented: postInitJeuR"
